e8bc7e8e3e29e5811817830d348ac761412208c4
Joaquim Oliveira <jjsfoliveira@gmail.com>
2013-12-22 19:57:07 -0800

    czxscs scsaca

--- java_screen/RobotScreen/src/MyPanel.java
+++ java_screen/RobotScreen/src/MyPanel.java
@@ -18,6 +18,9 @@ class MyPanel extends JPanel{
 	private static final long serialVersionUID = 1L;
 	static int[][] buffer = new int[Constants.height][Constants.width];
 	Image robo;
+	int robo_width;
+	int robo_height;
+	
     public MyPanel() {
     	setBorder(BorderFactory.createLineBorder(Color.black));
     	
@@ -27,6 +30,8 @@ class MyPanel extends JPanel{
  		   }
  	   }
     	robo = Toolkit.getDefaultToolkit().getImage("rob.png");
+    	robo_width = 34;
+    	robo_height = 34;
     }
 
 
@@ -36,12 +41,21 @@ class MyPanel extends JPanel{
     }
     
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g);       
+        super.paintComponent(g); 
+        
+    	
+        updateBuffer();
         BufferedImage bufferedImage = new BufferedImage(Constants.width,Constants.height, BufferedImage.TYPE_INT_RGB);
         for(int i = 0; i < Constants.height; i++){
         	for(int j = 0; j < Constants.width; j++){
-        		if(buffer[i][j] == 0){
+        		
+        		switch(buffer[i][j]){
+        		case 0:
         			bufferedImage.setRGB(j, i,(new Color(0, 0, 0)).getRGB());
+        			break;
+        		case 1:
+        			bufferedImage.setRGB(j, i,(new Color(255, 255, 255)).getRGB());
+        			break;
         		}
         	}
         }
@@ -53,12 +67,27 @@ class MyPanel extends JPanel{
         
         AffineTransform at = new AffineTransform();
 
-        at.translate(Constants.x_rob, Constants.y_rob);
+        at.translate(Constants.x_rob+robo_width/2+1, Constants.y_rob-robo_height/2+1);
         at.rotate(Math.toRadians(Constants.rot_rob));
-        
         Graphics2D g2d = (Graphics2D) g;
         g2d.drawImage(robo, at, null);
         
-    }  
+    } 
+    
+    public void updateBuffer(){
+    	for(int i = 0; i < Constants.height; i++){
+        	for(int j = 0; j < Constants.width; j++){
+        		if(buffer[i][j] == 1)
+        			continue;
+        		if(buffer[i][j] == 0){
+        			if(robo_height/2 >= Math.sqrt(((Constants.x_rob-j)*(Constants.x_rob-j))+
+        					((Constants.y_rob-i)*(Constants.y_rob-i)))){
+        				buffer[i][j] = 1;
+        			}
+        		}
+        	}
+        }
+    	this.repaint();
+    }
     
 }
--- java_screen/RobotScreen/src/RobotScreen.java
+++ java_screen/RobotScreen/src/RobotScreen.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.*;
@@ -33,13 +35,46 @@ public class RobotScreen
 	   }*/
 	   JFrame frame = new JFrame("Meu primeiro frame em Java");
       // frame.setSize(400,400);
-       
-       frame.add(new MyPanel());
+       final MyPanel panel = new MyPanel();
+       frame.add(panel);
+       panel.setFocusable(true);
+       panel.requestFocusInWindow();
+       panel.addKeyListener(new KeyListener() {
+		
+		@Override
+		public void keyTyped(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		@Override
+		public void keyReleased(KeyEvent e) {
+		//	panel.updateBuffer();
+		}
+		
+		@Override
+		public void keyPressed(KeyEvent e) {
+			System.out.println("pres");
+			switch(e.getKeyCode()){
+			case KeyEvent.VK_LEFT:
+				Constants.x_rob -= 2;
+				break;
+			case KeyEvent.VK_RIGHT:
+				Constants.x_rob += 2;
+				break;
+			case KeyEvent.VK_DOWN:
+				Constants.y_rob += 2;
+				break;
+			case KeyEvent.VK_UP:
+				Constants.y_rob -= 2;
+				break;
+			}
+			
+		}
+	});
        frame.pack();
        frame.setVisible(true);
-
-       
-	   
+  
    }
    
    
