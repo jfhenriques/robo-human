fb88bc06e91ba5f71d972f5c9ce5861f85d2ab96
Joaquim Oliveira <jjsfoliveira@gmail.com>
2013-12-29 17:59:51 -0800

    add robo automatico

---
+++ robo_AI/robsample/s5.sh
@@ -0,0 +1,5 @@
+L D_LIBRARY_PATH=../libRobSock/ ./robsample -pos 1 -robname "R1" &
+L D_LIBRARY_PATH=../libRobSock/ ./robsample -pos 2 -robname "R2" &
+L D_LIBRARY_PATH=../libRobSock/ ./robsample -pos 3 -robname "R3" &
+L D_LIBRARY_PATH=../libRobSock/ ./robsample -pos 4 -robname "R4" &
+L D_LIBRARY_PATH=../libRobSock/ ./robsample -pos 5 -robname "R5" & 
